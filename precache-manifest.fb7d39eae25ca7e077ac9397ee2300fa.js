93f4053d89637ca2a2acb424b41f959c9c2c52c7
Nirmal Kumar <codingboredom@gmail.com>
2020-03-12 00:36:34 +0530

    Updates

---
+++ precache-manifest.fb7d39eae25ca7e077ac9397ee2300fa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bcb2f7ea3da1483c491bcd3776da104a",
+    "url": "/reactjs-demo/index.html"
+  },
+  {
+    "revision": "38fafbe19aa04ed84467",
+    "url": "/reactjs-demo/static/css/main.e3a78b40.chunk.css"
+  },
+  {
+    "revision": "0d800027689dce008cd6",
+    "url": "/reactjs-demo/static/js/2.589b3e07.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/reactjs-demo/static/js/2.589b3e07.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "38fafbe19aa04ed84467",
+    "url": "/reactjs-demo/static/js/main.638c2022.chunk.js"
+  },
+  {
+    "revision": "d75c895766444cabb250",
+    "url": "/reactjs-demo/static/js/runtime-main.abc4be0b.js"
+  }
+]);
