c92f171e32d05d1107781685964de42a1b4ca50e
Nirmal Kumar <codingboredom@gmail.com>
2020-03-12 23:33:39 +0530

    Removed console logs

--- src/components/Button.js
+++ src/components/Button.js
@@ -2,7 +2,6 @@ import React, {Component} from "react";
 
 class Button extends Component {
     render() {
-        console.log("Button Props", this.props);
         return (
             <div className={`column-${this.props.cols}`}>
                 <button className="calc-button" onClick={() => this.props.action(this.props.symbol)}>{this.props.symbol}</button>
